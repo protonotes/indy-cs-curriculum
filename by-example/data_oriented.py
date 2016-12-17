7b54ae762daddb3708d3551e21b912b4754e9b21
Kay <Protosac@users.noreply.github.com>
2016-12-17 12:26:54 -0800

    Add data driven samples.

---
+++ by-example/data_oriented.py
@@ -0,0 +1,19 @@
+"""
+Data Oriented Design
+---------------------
+
+It's important to remember that no one approach is a silver bullet. The purpose of learning about different programming paradigms is so that you have the tools to apply the varioius approaches needed for your particular software. Paradigms are not mutuall exclusive; more often than not your programs will borrow ideas from each.
+
+Sources:
+1. https://dataorientedprogramming.wordpress.com/tag/mike-acton/
+2. http://www.dataorienteddesign.com/dodmain/
+
+
+Advantages:
+* Paralleization
+* Cache Utilization
+* Modularity
+* Testing
+
+"""
+
